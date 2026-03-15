834e36773042e891dda1754729666e745fbcfea3
Maaz-Ahmed <wwwmaazahmed1@gmail.com>
2026-03-15 18:32:11 +0500

    update
    update

--- app.js
+++ app.js
@@ -1,60 +1,121 @@
-let ListContainer = document.querySelector('.ListContainer')
-let addBtn = document.querySelector('.addBtn')
-let addInput = document.querySelector('#addInput')
+let container = document.querySelector('.container');
 
-const list = [
+container.addEventListener('click', (e) => {
+
+    if (e.target.classList.contains('Button1')) {
+        let one = e.target.closest('.maxContainer')
+        let two = one.querySelector('b')
+        let ok = two.classList.toggle('line-through')
+        if (ok) {
+
+            two.style.textDecoration = 'line-through';
+        } else {
+            two.style.textDecoration = 'none';
+
+        }
+
+    }
+
+})
+
+let lowerContainer = document.querySelector('.lowerContainer')
+let input1 = document.querySelector('.input1')
+let input2 = document.querySelector('.input2')
+let ok = document.querySelector('.ok')
+let arr = [
+    {
+        name: 'maaz',
+        email: `wwwmaazahmed1@gmail.com`
+    }
 ]
 
+function objec(ok) {
+    lowerContainer.innerHTML = ''
+    // arr.length = 0
+    ok.map(e => {
 
-function randerList(arr) {
-    ListContainer.innerHTML = ''
-    arr.map(item => {
-        let { id, text } = item
-        let divelem = document.createElement('div');
-        divelem.className = 'list'
-        divelem.innerHTML = `
-    <p>${text}</p>
-    `
-    let buttonElem = document.createElement('button');
-    buttonElem.innerText = '❌'
-    divelem.appendChild (buttonElem)
-    divelem.onclick = ()=>{
-        handleRemoveList(id)
-    } 
-    ListContainer.append(divelem)
+        let { name, email } = e
+        // let newElement = document.createElement('div');
+        let maxContainer = document.createElement('div')
+        maxContainer.className = 'maxContainer';
+        maxContainer.innerHTML = `   <div class="image">
+        <img src="Screenshot 2026-02-17 003549.png" alt="mnmvf">
+        </div>
+        <div class="text">
+        <h3>${name}</h3>
+        <p><b class = lineThroughing>${email}</b></p>
+        </div>`
+
+
+        let secNewCon = document.createElement('div')
+        let button = document.createElement('button');
+        let button1 = document.createElement('button')
+        button1.className = 'Button1'
+        button1.innerHTML = '✍️'
+        button.className = `Button2`;
+        button.innerHTML = '⚔️'
+        secNewCon.append(button, button1)
+        secNewCon.className = 'Btn'
+        maxContainer.append(secNewCon)
+        button.addEventListener('click', () => {
+            deleting(name)
+        })
+        // button1.addEventListener('click', (e) => {
+        //     let one = e.target.closest('.maxContainer')
+        //     one.classList.toggle('one')
+        //     let two = one.querySelector('.text')
+        //     let three = two.querySelector('p')
+        //     if (one.className === 'maxContainer') {
+        //         three.style.textDecoration = 'line-through'
+
+        //     } else {
+        //         three.style.textDecoration = 'none'
+
+        //     }
+
+        // })
+        lowerContainer.append(maxContainer)
     })
 }
-randerList(list)
-function handleAddList(){
-    if(addInput.value.trim()===''){
-        alert('pleace enter something first')
-    }else{ 
-    let obj = {
-        id: Date.now(),
-        text:addInput.value
+function secBtn(e) {
+
+
+}
+
+objec(arr)
+function Adding(ok) {
+    if (input1.value.trim() === '') {
+        // alert('kjnvf')
+        return
     }
-    list.unshift (obj)
-    randerList(list)
-    addInput.value = ''
+
+    let okk = {
+        name: input1.value,
+        email: input2.value,
+    }
+    lowerContainer.innerHTML = ''
+    arr.unshift(okk)
+    objec(arr)
+    input1.value = '';
+    input2.value = '';
 }
-}
-function handleRemoveList(id){
-    const filtredlist = list.filter(obj =>{
-        return obj.id !== id
+ok.addEventListener('click', Adding)
+
+function filtering() {
+    let triger = input1.value
+    let okkk = arr.filter(e => {
+        return e.name.toLowerCase().includes(triger.toLowerCase()) ||
+            e.email.toLowerCase().includes(triger.toLowerCase())
     })
-    list.length = 0;
-    list.push(...filtredlist)
-    randerList(filtredlist)
+    objec(okkk)
 }
-addBtn.addEventListener('click',handleAddList)
+input1.addEventListener('input', filtering)
 
-
-// let arr = [1,3,4]
-// // arr.length = 0
-// arr = []
-// console.log(arr);
-
-// let a = []
-// let b = [2,4,6]
-// let ok = a.push(...b)
-// console.log(ok);
+function deleting(id) {
+    let delet = arr = arr.filter(e => {
+        return e.name !== id
+    })
+    objec(delet)
+}
+// let Button2 = document.querySelector('.Button2')
+// // Button2.addEventListener('click',deleting(arr.name))
